19b22fe321834f48102e8d062e96fbb8aa4db1ba
Kaustubh Hiware <hiwarekaustubh@gmail.com>
2026-02-21 12:58:18 +0900

    skeleton TTS generation

--- LOG.md
+++ LOG.md
@@ -6,3 +6,14 @@
 2. character definition (traits, attributes) -> if some character is already saved, define a new one or use the existing one
 2a. the new character popup (modal): character nickname box, character image upload, voice tuning dials (5 options but let's make only 1 work for the demo: let's use very non-technical about these options, such a heacy-light voice, are they sarcastic/funny/rude personality, etc.) make few voice samples and let them decide on some.
 3. third screen: thumbnails with 1 row and 3 columns of the videos generated (all will be different thumbnails), and when clicked, the videos can be played, and each vid has a download button
+
+
+
+## Voice generation (12:20 PM)
+
+1. Mapping voice to personalities - [docs](https://discord.com/channels/@me/1474037988890644634/1474608395553079497)
+2. API sample for text to voice clip
+   1. Add `GEMINI_API_KEY` in .env.
+   2. `npm install`
+   3. `npx tsx script-to-voice.ts`
+   4. `play influencer-audio.wav`
---
+++ script-to-voice.ts
@@ -0,0 +1,62 @@
+import {GoogleGenAI} from '@google/genai';
+import wav from 'wav';
+import 'dotenv/config';
+
+async function saveWaveFile(
+   filename: string,
+   pcmData: Buffer,
+   channels: number = 1,
+   rate: number = 24000,
+   sampleWidth: number = 2,
+): Promise<void> {
+   return new Promise((resolve, reject) => {
+      const writer = new wav.FileWriter(filename, {
+            channels,
+            sampleRate: rate,
+            bitDepth: sampleWidth * 8,
+      });
+
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+
+      writer.write(pcmData);
+      writer.end();
+   });
+}
+
+async function main() {
+   const ai = new GoogleGenAI({
+      apiKey: process.env.GEMINI_API_KEY,
+   });
+
+   const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash-preview-tts",
+      contents: [{
+         parts: [{
+            text: 'Say with a sense of urgency: Download GAMENAME now, or else!'
+         }],
+      }],
+      config: {
+            responseModalities: ['AUDIO'],
+            speechConfig: {
+               voiceConfig: {
+                  prebuiltVoiceConfig: { voiceName: 'Kore' },
+                  // TODO: use internally mapped voice name for the character here.
+               },
+            },
+      },
+   });
+
+   const data = response.candidates?.[0]?.content?.parts?.[0]?.inlineData?.data;
+   
+   if (!data) {
+      throw new Error('No audio data received from API response');
+   }
+   
+   const audioBuffer = Buffer.from(data, 'base64');
+
+   const fileName = 'influencer-audio.wav';
+   await saveWaveFile(fileName, audioBuffer);
+}
+
+main().catch(console.error);
